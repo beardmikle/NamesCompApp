fe4bfad53586001a28c1f68422ddfe1c8777f538
beardmikle <ankmo@ya.ru>
2023-01-03 21:58:22 +0300

    code 1

---
+++ NamesCompApp/FirstViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  NamesCompApp
+//
+//  Created by beardmikle on 02.01.2023.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var yourNameTF: UITextField!
+    @IBOutlet weak var partnerNameTF: UITextField!
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard  let destinationVC = segue.destination as? ResultViewController else { return }
+        destinationVC.firstName = yourNameTF.text
+        destinationVC.secondName = partnerNameTF.text
+    }
+    
+    @IBAction func resultButtonTapped() {
+        guard let firstName = yourNameTF.text, let secondName = partnerNameTF.text else { return }
+        if firstName.isEmpty || secondName.isEmpty {
+            showAlert(
+                title: "❗️ Names are missing ❗️",
+                message: "Please enter both name 😕"
+            )
+         return
+        }
+        performSegue(withIdentifier: "goToResult", sender: nil)
+        
+    }
+    
+    @IBAction func unwindSegueToFirstVS(segue: UIStoryboardSegue) {
+        yourNameTF.text = ""
+        partnerNameTF.text = ""
+    }
+}
+extension FirstViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+
+extension FirstViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == yourNameTF {
+            partnerNameTF.becomeFirstResponder()
+        } else {
+            resultButtonTapped()
+        }
+        return true
+    }
+    
+}
